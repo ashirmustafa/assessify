1c6b3c7ca5c718f4919882e1219e3caa2e1eb994
Ashir Mustafa <100285531+ashirmustafa@users.noreply.github.com>
2024-04-22 19:39:52 +0500

    password match func fixed

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -50,11 +50,10 @@ const Page = () => {
         username: z.string().min(6, { message: 'Username must be at least 6 characters' }),
         workEmail: z.string().email(),
         password: z.string().min(5, { message: 'Password must be at least 5 characters' }),
-        confirmPassword: z.string().min(5),
-        conditionTerms: z.boolean()
-    }).refine((data)=> data.password === data.confirmPassword, {
+        confirmPassword: z.string().min(5)
+    }).refine((data) => data.password === data.confirmPassword, {
         message: 'Passwords do not match',
-        path: ["confirmPassword"],
+        path: ["confirmPassword"]
     })
     const form = useForm({
         resolver: zodResolver(formSchema),
